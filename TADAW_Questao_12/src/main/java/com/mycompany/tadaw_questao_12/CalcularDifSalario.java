9f31a89561cf9bc8fe772fb1bbf072c5249417b8
Leandro Lima <lecoreislima_z2@hotmail.com>
2026-03-23 14:48:24 -0300

    Adicionando questões de Desenvolvimento APP WEB - JAVA

---
+++ TADAW_Questao_12/src/main/java/com/mycompany/tadaw_questao_12/CalcularDifSalario.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.tadaw_questao_12;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author leo
+ */
+@WebServlet(name = "CalcularDifSalario", urlPatterns = {"/CalcularDifSalario"})
+public class CalcularDifSalario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        double salarioA = Double.parseDouble(request.getParameter("salarioA"));
+        double salarioB = Double.parseDouble(request.getParameter("salarioB"));
+        double salarioC = Double.parseDouble(request.getParameter("salarioC"));
+        
+        double maior;
+        double menor;
+        
+        //Maior salário
+        if (salarioA > salarioB && salarioA > salarioC){
+            maior = salarioA;
+        }
+        else if (salarioB > salarioA && salarioB > salarioC){
+            maior = salarioB;
+        }
+        else{
+            maior = salarioC;
+        }
+        
+        //Menor salário
+        if (salarioA < salarioB && salarioA < salarioC){
+            menor = salarioA;
+        }
+        else if (salarioB < salarioA && salarioB < salarioC){
+            menor = salarioB;
+        }
+        else{
+            menor = salarioC;
+        }
+        
+        //Diferença salário percentual
+        double dif = ((maior - menor) / menor) * 100;
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CalcularDifSalario</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Resultado: </h1>");
+            out.printf("<p>Maior salário: %.2f</p>", maior);
+            out.printf("<p>Menor salário: %.2f</p>", menor);
+            out.printf("<p>Diferença percentual: %.2f%%</p>", dif);
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
